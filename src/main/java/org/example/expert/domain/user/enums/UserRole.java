d9921687f8ffae019c430c1f42d824703991bfc2
박병천 <genius178896@gmail.com>
2025-01-25 00:26:47 +0900

    Refactor : Lv2 - 9 SpringSecurity 사용을 위해 시큐리티 특성상 권한은 ROLE_을 붙이는 것 때문에 제거해주는 로직을 추가해줌

--- src/main/java/org/example/expert/domain/user/enums/UserRole.java
+++ src/main/java/org/example/expert/domain/user/enums/UserRole.java
@@ -7,10 +7,19 @@ import java.util.Arrays;
 public enum UserRole {
     ADMIN, USER;
 
+
+    /**
+     * 시큐리티에서 인가를 ROLE_ADMIN or ROLE_USER로 반환해주기 때문에 여기서 ROLE_을 제거해주는 로직을 추가했습니다.
+     */
     public static UserRole of(String role) {
-        return Arrays.stream(UserRole.values())
-                .filter(r -> r.name().equalsIgnoreCase(role))
-                .findFirst()
-                .orElseThrow(() -> new InvalidRequestException("유효하지 않은 UerRole"));
+        if (role.startsWith("ROLE_")) {
+            role = role.substring(5);
+        }
+        for (UserRole userRole : UserRole.values()) {
+            if (userRole.name().equalsIgnoreCase(role)) {
+                return userRole;
+            }
+        }
+        throw new IllegalStateException("잘못된 enum 값입니다");
     }
 }
